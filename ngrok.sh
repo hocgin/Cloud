419d246569ed5eae8aa566b1d60e265be34ed179
hocgin <hocgin@gmail.com>
2016-12-05 17:46:45 +0800

    - add ngrok support

---
+++ ngrok.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+subDomain=cloud
+port=8080
+dir=/home/hocgin/Documents/NutzStore/Nutstore/toolBox/ngrok
+echo $dir/bin/ngrok
+$dir/bin/ngrok -subdomain $subDomain -proto=http -config=$dir/bin/ngrok.cfg $port
