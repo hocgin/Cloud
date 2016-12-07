5728b112e8f6c8206d9da00f52f3721b81a80dc8
hocgin <hocgin@gmail.com>
2016-12-07 10:56:03 +0800

    - add MainSetup about info

--- src/main/java/in/hocg/MainSetup.java
+++ src/main/java/in/hocg/MainSetup.java
@@ -23,6 +23,7 @@ import static in.hocg.MainModule.FORCE_CREATE_TABLE;
 /**
  * (๑`灬´๑)
  * Created by hocgin on 十一月28  028.
+ * Tomcat 初始化时执行..
  */
 public class MainSetup implements Setup {
     private static final Log log = Logs.get();
@@ -36,14 +37,20 @@ public class MainSetup implements Setup {
         _initRedis(ioc);
         _initQuartz(ioc);
         _initAuthority(dao, ioc);
+        
+        
+	    log.info(String.format("[%s] 运行结束, Bye.", "MainSetup 初始化"));
     }
 	
 	/**
-	 * 初始化权限
+	 * - 启用 [@RequiresPermissions, @RequiresRoles]
+	 * - admin权限检查
 	 * @param dao
 	 * @param ioc
 	 */
 	private void _initAuthority(Dao dao, Ioc ioc) {
+		log.info(String.format("正在初始化[%s]..", "权限检查模块"));
+		
 	    Role role = dao.fetch(Role.class, Cnd.where("name", "=", "admin"));
         Record fetch = dao.fetch("t_user_role", Cnd.where("role_id", "=", role.getId()));
         if (!fetch.isEmpty()) {
@@ -63,10 +70,12 @@ public class MainSetup implements Setup {
     }
 
     /**
-     * 初始化数据库
+     * 初始化数据库数据
      * @param ioc
      */
     private void _loadSenders(Ioc ioc) {
+	    log.info(String.format("正在初始化[%s]..", "数据库数据"));
+	    
         ioc.get(MainSeeder.class).handler();
     }
 
@@ -75,6 +84,8 @@ public class MainSetup implements Setup {
      * @param ioc
      */
     private void _initRedis(Ioc ioc) {
+	    log.info(String.format("正在初始化[%s]..", "Redis"));
+	    
         SettingService service = ioc.get(SettingService.class);
         RedisService redis = ioc.get(RedisService.class);
         for (Setting setting : service.query(Setting.Tag.Redis.name())) {
@@ -87,6 +98,8 @@ public class MainSetup implements Setup {
      * @param ioc
      */
     private void _initQuartz(Ioc ioc) {
+	    log.info(String.format("正在初始化[%s]..", "定时任务列表"));
+	    
         ioc.get(NutQuartzCronJobFactory.class);
     }
 }
