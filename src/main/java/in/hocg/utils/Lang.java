15cecf75dca90b9cd777e11b1c99c971dbd4381d
hocgin <hocgin@gmail.com>
2016-12-07 10:36:47 +0800

    - add utils package

---
+++ src/main/java/in/hocg/utils/Lang.java
@@ -0,0 +1,28 @@
+package in.hocg.utils;
+
+/**
+ * Created by hocgin on 16-12-7.
+ */
+public class Lang {
+	
+	/**
+	 * exp?true:false;
+	 * @param t
+	 * @param def
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> T ifNull(T t, T def) {
+		return t == null ? def : t;
+	}
+	
+	/**
+	 * exp?true:false;
+	 * @param str
+	 * @param def
+	 * @return
+	 */
+	public static String ifEmpty(String str, String def) {
+		return StringKit.isEmpty(str) ? def : str;
+	}
+}
---
+++ src/main/java/in/hocg/utils/StringKit.java
@@ -0,0 +1,16 @@
+package in.hocg.utils;
+
+/**
+ * Created by hocgin on 16-12-7.
+ */
+public class StringKit {
+	
+	/**
+	 * "" or null
+	 * @param str
+	 * @return
+	 */
+	public static boolean isEmpty(String str) {
+		return str == null || str.length() == 0;
+	}
+}
